490fd99715edde94661bd676ee430a350620e951
GTXDread <frans.sutjianto@binus.ac.id>
2023-12-20 13:00:52 +0700

    Penambahan Method Penghitungan Kolektibilitas
    Menambahkan method kolektibilitas ke controller (belum disambung ke view kolektibilitas).

--- app/Controllers/Kolektibilitas.php
+++ app/Controllers/Kolektibilitas.php
@@ -83,4 +83,94 @@ class Kolektibilitas extends BaseController
         session()->setFlashdata('success', 'Data Berhasil di Hapus');  // Menampilkan pesan sukses.
         return redirect()->to(base_url('kolektibilitas'));  // Mengalihkan ke halaman Kolektibilitas.
     }
+
+    public function hitung_kolektibilitas($no_kontrak)
+    {
+        // Method untuk menghitung kolektibilitas berdasarkan nomor kontrak
+        
+        $model = model(KolektibilitasModel::class);
+        
+        // Pokok dan jasa masih belum ada tabelnya
+        $pokok = 0;
+        $jasa = 0;
+
+        for($i = 1; $i = $model->where('angsuran_pokok !=', 0)->countAllResults(); $i++)
+        {
+            $tanggal = new DateTime($model->select('tanggal_cicilan')->where('no_kontrak', $no_kontrak)->where('no_cicilan', $i));
+            $cicilan = $model->where('angsuran_pokok !=', 0)->where('no_kontrak', $no_kontrak)->where('no_cicilan', $i)->get();
+
+            if(empty($tanggalLunas))
+            {
+                $selisihHari = (empty($tanggalSebelumnya) ? 0 : $tanggal->diff($tanggalSebelumnya)->days);
+            } else {
+                $selisihHari = (empty($tanggalLunas) ? 0 : $tanggal->diff($tanggalLunas)->days);
+            }
+            $cicilan = $cicilan - $jasa - $selisih;
+
+            if($cicilan == $pokok || $cicilan > $pokok)
+            {
+                $selisih = 0;
+            } else if($cicilan < $pokok) {
+                $selisih = $pokok - $cicilan;
+            }
+        
+            if($selisih != 0)
+            {
+                if($cicilanSebelumnya + $selisihSebelumnya == pokok && $cicilan > 0)
+                {
+                    $tanggalLunas = $tanggalSebelumnya;
+                } else if($cicilan < 0)
+                {
+                    if(empty($tanggalLunas))
+                    {
+                        $tanggalLunas = $tanggalSebelumnya;
+                    }
+                    $selisihHari = (empty($tanggalLunas) ? 0 : $tanggal->diff($tanggalLunas)->days);
+                }
+            } else {
+                if($selisihSebelumnya != 0)
+                {
+                    $tanggalLunas = $tanggalSebelumnya;
+                    $selisihHari = (empty($tanggalLunas) ? 0 : $tanggal->diff($tanggalLunas)->days); 
+                }
+                $tanggalLunas = $tanggal;
+            }
+        
+            if($selisihHari <= 30)
+            {
+                $kolek = 1;
+            } else if($selisihHari > 30 && $selisihHari <= 90) {
+                $kolek = 2;
+            } else if($selisihHari > 90 && $selisihHari <= 180) {
+                $kolek = 3;
+            } else if($selisihHari > 180) {
+                $kolek = 4;
+            }
+    
+            // If untuk mengecek situasi dimana kolektibilitas hanya dapat naik perlahan
+            if($kolek < $kolekSebelumnya)
+            {
+                $kolek = $kolekSebelumnya - 1;
+        
+                // If untuk mengecek cicilan parsial sehingga kolektabilitas tidak dapat naik
+                if($selisihSebelumnya != 0 && $selisih != 0 && $kolek < 4)
+                {
+        	        $kolek = $kolekSebelumnya;
+		        }
+            }
+                // If untuk mengecek perubahan cicilan full menjadi cicilan parsial sehingga kolektibilitas naik satu
+            if($selisihSebelumnya == 0 && $selisih != 0 && $kolekSebelumnya < 4)
+            {
+                $kolek+=1;
+            }
+
+            $kolekSebelumnya = $kolek;
+            $cicilanSebelumnya = $cicilan;
+            $tanggalSebelumnya = $tanggal;
+            $selisihSebelumnya = $selisih;
+        }
+        session()->setFlashdata('success', 'Kolektibilitas Berhasil Di Hitung');
+
+        return $kolek;
+    }
 }
